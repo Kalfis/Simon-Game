5f530b4b143ba112da296701642ee9fe8c45cd34
William Chin <william-chin@outlook.com>
2015-10-15 14:16:19 -0400

    player input solved

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
 
 var randomSequence = function() {
-  var cpuArray = [Math.floor((Math.random()*4)+1), Math.floor((Math.random()*4)+1), Math.floor((Math.random()*4)+1), Math.floor((Math.random()*4)+1)];
-  return cpuArray
+  var cpuColors = [Math.floor((Math.random()*4)+1), Math.floor((Math.random()*4)+1), Math.floor((Math.random()*4)+1), Math.floor((Math.random()*4)+1)];
+  return cpuColors;
 }
-randomSequence();
 
-var redButton = $('<span>');
+var cpuArray = randomSequence();
+
+var redButton = $('<button/>');
 redButton.addClass('red');
 
-var blueButton = $('<span>');
+var blueButton = $('<button/>');
 blueButton.addClass('blue');
 
-var yellowButton = $('<span>');
+var yellowButton = $('<button/>');
 yellowButton.addClass('yellow');
 
-var greenButton = $('<span>');
+var greenButton = $('<button/>');
 greenButton.addClass('green');
+
+var playerColors = new Array();
+
+$('.red').click(function(){
+  console.log('red was clicked');
+  playerColors.push(1)
+});
+
+$('.blue').click(function(){
+  console.log('blue was clicked');
+  playerColors.push(2)
+});
+
+$('.yellow').click(function(){
+  console.log('yellow was clicked');
+  playerColors.push(3)
+});
+
+$('.green').click(function(){
+  console.log('green was clicked');
+  playerColors.push(4)
+});
